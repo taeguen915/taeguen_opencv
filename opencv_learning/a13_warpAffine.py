21813560fdf58122afafae5764cefda42c21feac
taeguen915 <taeguen915@naver.com>
2024-01-17 17:17:16 +0900

    test

--- opencv_learning/a13_warpAffine.py
+++ opencv_learning/a13_warpAffine.py
@@ -1,15 +1,17 @@
 import cv2
-
+import numpy as np
 def main():
     cap = cv2.VideoCapture(0)
     con = True
     M1 = cv2.getRotationMatrix2D((0, 0), 45, 1)
     M1[0][2] += 200
     M1[1][2] += 200
+    M2 = cv2. getAffineTransform(np.float32([[0,0],[0,100],[100,0]]), np.float32([[70,0],[70, 200],[170, 0]]))
+    # type: ignore
     while con:
         ret, frame = cap.read()
         if ret:
-            frame = cv2.warpAffine(frame, M1, (frame.shape[1],frame.shape[0]))
+            frame = cv2.warpAffine(frame, M2, (frame.shape[1],frame.shape[0]))
             cv2.imshow("frame", frame)
         if cv2.waitKey(1) == ord("q"):
                 con = False
